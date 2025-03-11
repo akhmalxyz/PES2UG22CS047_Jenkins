a990fe74cd1a26c985d735dd46715e819a654146
Akhmal <amohammedt@gmail.com>
2025-03-11 18:32:30 +0530

    Updated hello.cpp with [brief description of changes]

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins from Akhmal\n";
+  cout << "Hello, World, what's up?\n";
+  cout << "Hello, Jenkins from Akhmal, SRN 047 EC Campus\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
